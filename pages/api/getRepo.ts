14ef8bfe25f23d0320e415db2fdd3f694241bc9d
harshit54 <harshitjoshi112@gmail.com>
2022-07-30 11:14:54 +0530

    Added support for rubygems

--- pages/api/getRepo.ts
+++ pages/api/getRepo.ts
@@ -10,6 +10,7 @@ type Data = {
 enum PackageRegistries {
   npm = "npm",
   pypi = "pypi",
+  rubygems = "rubygems",
 }
 
 type RepoInfo = {
@@ -29,10 +30,13 @@ export default async function handler(
 
   switch (type) {
     case PackageRegistries.npm:
-      repoInfo = await npmToGithub(link)
+      repoInfo = await npmToGithub(link);
       break;
-  case PackageRegistries.pypi:
-    repoInfo = await pypiToGitHub(link)
+    case PackageRegistries.pypi:
+      repoInfo = await pypiToGitHub(link);
+      break;
+    case PackageRegistries.rubygems:
+      repoInfo = await rubygemsToGitHub(link);
   }
 
   const repoData = await octokit.request(
@@ -45,7 +49,7 @@ export default async function handler(
   const ownerInfo = await octokit.request(`GET /users/${repoInfo.user}`, {
     username: repoInfo.user,
   });
-  
+
   res.status(200).json({ repoData: repoData, ownerInfo: ownerInfo });
 }
 
@@ -53,6 +57,7 @@ async function npmToGithub(link: string): Promise<RepoInfo> {
   let parsedLink = url.parse(link);
   let packageName = parsedLink.path?.split("/")[2];
   let registryURL = "https://registry.npmjs.com/" + packageName;
+  console.log("NPM Registry URL: ", registryURL);
   let response: any = await axios.get(registryURL);
   let repoURL = response.data.repository.url;
   let parsedURL = url.parse(repoURL);
@@ -68,10 +73,25 @@ async function pypiToGitHub(link: string): Promise<RepoInfo> {
   if (link.slice(-1) == "/") link += "json";
   else link += "/json";
   let registryURL = link;
-  console.log("Registry URL", registryURL);
+  console.log("PYPI Registry URL: ", registryURL);
   let response: any = await axios.get(registryURL);
-  // console.log("Reponse: ", response);
   let repoURL = response.data.info.project_urls["Source Code"];
+  let parsedURL = url.parse(repoURL);
+  let parsedPath = parsedURL.path!.split("/");
+  let user = parsedPath[1];
+  let repo = parsedPath[2];
+  if (repo.slice(-4) == ".git") repo = repo.slice(0, -4);
+  return { user: user, repoName: repo };
+}
+
+async function rubygemsToGitHub(link: string): Promise<RepoInfo> {
+  if (link[link.length - 1] == "/") link = link.slice(0, -1);
+  let linkSplit = link.split("/");
+  let packageName = linkSplit[linkSplit.length - 1];
+  let registryURL = "https://rubygems.org/api/v1/gems/" + packageName + ".json";
+  console.log("RubyGems Registry URL", registryURL);
+  let response: any = await axios.get(registryURL);
+  let repoURL = response.data.source_code_uri;
   let parsedURL = url.parse(repoURL);
   let parsedPath = parsedURL.path!.split("/");
   let user = parsedPath[1];
